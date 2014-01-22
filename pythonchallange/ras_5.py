74e476f6550094bdef6b72cf2179f7efa5ff1134
Rassilion <denizgezgin365@gmail.com>
2014-01-22 20:01:17 +0200

    pythonchallange 5
    pythonchallange 5 düzeltildi

--- pythonchallange/ras_5.py
+++ pythonchallange/ras_5.py
@@ -5,10 +5,10 @@ nothing = 12345
 while True:
     url = urllib2.urlopen("http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%s" %nothing)
     sayfa = url.read()
-    ara = re.search("\d+\d",sayfa)
+    ara = re.search("\d+\d$",sayfa) # 82683 yaniltmasindan kacmak icin $ eklendi
     if ara:
         nothing = ara.group()
         print nothing
     else:
-        print nothing
+        print sayfa
         break
