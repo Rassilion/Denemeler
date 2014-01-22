91b8d2fefe340fc96b89bbedc8f0542fb27824c5
Rassilion <denizgezgin365@gmail.com>
2014-01-22 23:43:56 +0200

    pythonchallange 6
    pythonchallange 6

--- pythonchallange/ras_6.py
+++ pythonchallange/ras_6.py
@@ -7,13 +7,17 @@ data = {}
 for name in file.namelist():
     data[name] = file.read(name)
 
-nothing = 100
+nothing = 90052
+comment=[]
 while True:
     a = str(nothing)+".txt"
     ara = re.search("\d+\d$", data[a])
     if ara:
         nothing = ara.group()
+        comment.append(file.getinfo(a).comment)
         print nothing
     else:
         print data[a]
         break
+
+print "".join(comment)
